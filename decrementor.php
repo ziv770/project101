03b3fc19a33e3b50bacf33b3bdc99c241f890975
ziv770 <ziv@usa.com>
2025-01-15 21:19:50 +0200

    sixth commit

---
+++ decrementor.php
@@ -0,0 +1,15 @@
+<?php
+$i=4;
+echo $i . "<br>";
+$i-=1; /*first way if decreasing by 1*/
+echo $i . "<br>";
+$i--;  /*second way if decreasing by 1*/
+echo $i . "<br>";
+$i=$i-1; /*third way if decreasing by 1*/
+echo $i;
+
+
+
+
+
+?>
--- incrementor.php
+++ incrementor.php
@@ -1,4 +1,13 @@
 <?php
+$i=4;
+echo $i . "<br>";
+$i+=1; /*first way if increasing by 1*/
+echo $i . "<br>";
+$i++;  /*second way if increasing by 1*/
+echo $i . "<br>";
+$i=$i+1; /*third way if increasing by 1*/
+echo $i;
+
 
 
 
